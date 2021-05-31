79e91933d70cd0212ee4a2af2b6ec107d9861d35
김영기 <dud0944@naver.com>
2021-05-31 20:44:25 +0900

    heroku index commit
    heroku는 php로 인식함

---
+++ index.php
@@ -0,0 +1,18 @@
+{\rtf1\ansi\ansicpg949\cocoartf2578
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Menlo-Bold;\f1\fnil\fcharset0 Menlo-Regular;}
+{\colortbl;\red255\green255\blue255;\red135\green135\blue135;\red245\green245\blue245;\red38\green38\blue38;
+\red14\green114\blue164;\red210\green0\blue53;}
+{\*\expandedcolortbl;;\cssrgb\c60000\c60000\c60000;\cssrgb\c96863\c96863\c96863;\cssrgb\c20000\c20000\c20000;
+\cssrgb\c0\c52549\c70196;\cssrgb\c86667\c6667\c26667;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\sl400\partightenfactor0
+
+\f0\b\fs28\fsmilli14450 \cf2 \cb3 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 <?
+\f1\b0 \cf4 \strokec4 \
+    \cf5 \strokec5 header\cf4 \strokec4 (\cf6 \strokec6 'Location: /index.html'\cf4 \strokec4 );\
+
+\f0\b \cf2 \strokec2 ?>
+\f1\b0 \cf4 \strokec4 \
+}
